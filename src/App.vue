06a1c28e824a208a817a49eb5f928fde20fc71c4
Den Kuznetsov <webrote@yandex.ru>
2019-07-11 18:11:50 +0300

    Vue-cli project first

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="app">
+    <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+      <a class="navbar-brand" href="#">Fixed navbar</a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarCollapse"
+        aria-controls="navbarCollapse"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarCollapse">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <router-link to="/" class="nav-link">Главная</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link to="/users" class="nav-link">Список пользователей</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link to="/add" class="nav-link">Добавление пользователя</router-link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+
+    <main role="main" class="container">
+      <router-view />
+    </main>
+
+    <!--  <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div> -->
+  </div>
+</template>
+
+<style>
+#app {
+  margin-top: 60px;
+}
+</style>
---
+++ src/views/Users.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h2>Список пользователей</h2>
+
+    <div v-if="!users.length" class="alert alert-warning">
+      Загрузка...
+    </div>
+    <users-list v-else :users="users"></users-list>
+  </div>
+</template>
+
+<script>
+
+import UsersList from '@/components/UsersList.vue'
+
+export default {
+  name: 'UsersList',
+  components: {
+    'users-list': UsersList
+  },
+  data: () => ({
+    users: []
+  })
+}
+</script>
